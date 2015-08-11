d361225ff9292543e53d7b9c661fa87eba5cfa5c
Erik Sargent <erik@lightningkite.com>
2015-08-11 13:53:49 -0600

    Fixing test target

--- Raven-SwiftTests/RavenConfigTests.swift
+++ Raven-SwiftTests/RavenConfigTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 class RavenConfigTests: XCTestCase {
-
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,59 +18,71 @@ class RavenConfigTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
-    func testSetDSNWithPort()
-    {
-        var config = RavenConfig(DSN: "http://public_key:secret_key@example.com:8000/project-id")
-        let didParse = config != nil
-        
-        XCTAssert(didParse, "Failed to parse DSN")
-        
-        XCTAssert(config!.publicKey! == "public_key", "Got incorrect publicKey \(config!.publicKey!)")
-        XCTAssert(config!.secretKey! == "secret_key", "Got incorrect secretKey \(config!.secretKey!)")
-        XCTAssert(config!.projectId! == "project-id", "Got incorrect projectId \(config!.projectId!)")
-        
-        let expectedURL = "http://example.com:8000/api/project-id/store/"
-        
-        XCTAssert(config!.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config!.serverUrl!.absoluteString!)")
+    
+    func testSetDSNWithPort() {
+        if let config = RavenConfig(DSN: "http://public_key:secret_key@example.com:8000/project-id") {
+            
+            XCTAssert(config.publicKey! == "public_key", "Got incorrect publicKey \(config.publicKey!)")
+            XCTAssert(config.secretKey! == "secret_key", "Got incorrect secretKey \(config.secretKey!)")
+            XCTAssert(config.projectId! == "project-id", "Got incorrect projectId \(config.projectId!)")
+            
+            let expectedURL = "http://example.com:8000/api/project-id/store/"
+            
+            XCTAssert(config.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config.serverUrl!.absoluteString!)")
+        }
+        else {
+            XCTFail("Failed to parse DSN")
+        }
     }
     
-    func testSetDSNWithSSLPortUndefined()
-    {
-        
-        var config = RavenConfig(DSN: "https://public_key:secret_key@example.com/project-id")
-        let didParse = config != nil
-        
-        XCTAssert(didParse, "Failed to parse DSN")
-        
-        XCTAssert(config!.publicKey! == "public_key", "Got incorrect publicKey \(config!.publicKey!)")
-        XCTAssert(config!.secretKey! == "secret_key", "Got incorrect secretKey \(config!.secretKey!)")
-        XCTAssert(config!.projectId! == "project-id", "Got incorrect projectId \(config!.projectId!)")
+    func testSetDSNWithSSLPortUndefined() {
+        if let config = RavenConfig(DSN: "https://public_key:secret_key@example.com/project-id") {
 
-        
-        let expectedURL = "https://example.com:443/api/project-id/store/"
-        
-        XCTAssert(config!.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config!.serverUrl!.absoluteString!)")
-
-        
+            XCTAssert(config.publicKey! == "public_key", "Got incorrect publicKey \(config.publicKey!)")
+            XCTAssert(config.secretKey! == "secret_key", "Got incorrect secretKey \(config.secretKey!)")
+            XCTAssert(config.projectId! == "project-id", "Got incorrect projectId \(config.projectId!)")
+            
+            let expectedURL = "https://example.com:443/api/project-id/store/"
+            
+            XCTAssert(config.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config.serverUrl!.absoluteString!)")
+        }
+        else {
+            XCTFail("Failed to parse DSN")
+        }
     }
     
-    func testSetDSNWithoutSSLPortUndefined()
-    {
-        
-        var config = RavenConfig(DSN: "http://public_key:secret_key@example.com/project-id")
-        let didParse = config != nil
-        
-        XCTAssert(didParse, "Failed to parse DSN")
-        
-        XCTAssert(config!.publicKey! == "public_key", "Got incorrect publicKey \(config!.publicKey!)")
-        XCTAssert(config!.secretKey! == "secret_key", "Got incorrect secretKey \(config!.secretKey!)")
-        XCTAssert(config!.projectId! == "project-id", "Got incorrect projectId \(config!.projectId!)")
+    func testSetDSNWithoutSSLPortUndefined() {
+        if let config = RavenConfig(DSN: "http://public_key:secret_key@example.com/project-id") {
 
-        
-        let expectedURL = "http://example.com:80/api/project-id/store/"
-        
-        XCTAssert(config!.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config!.serverUrl!.absoluteString!)")
-        
+            XCTAssert(config.publicKey! == "public_key", "Got incorrect publicKey \(config.publicKey!)")
+            XCTAssert(config.secretKey! == "secret_key", "Got incorrect secretKey \(config.secretKey!)")
+            XCTAssert(config.projectId! == "project-id", "Got incorrect projectId \(config.projectId!)")
+            
+            let expectedURL = "http://example.com:80/api/project-id/store/"
+            
+            XCTAssert(config.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config.serverUrl!.absoluteString!)")
+        }
+        else {
+            XCTFail("Failed to parse DSN")
+        }
+    }
+    
+    func testSetDSNWithoutAuthentication() {
+        if let config = RavenConfig(DSN: "https://example.com/project-id") {
+            
+            let expectedURL = "https://example.com:443/api/project-id/store/"
+            XCTAssert(config.serverUrl!.absoluteString! == expectedURL, "Got incorrect serverURL \(config.serverUrl!.absoluteString!)")
+            
+            XCTAssertEqual(config.publicKey!, "", "Got incorrect publicKey \(config.publicKey)")
+            XCTAssertEqual(config.secretKey!, "", "Got incorrect secretKey \(config.secretKey)")
+        }
+        else {
+            XCTFail("Failed to parse DSN")
+        }
+    }
+    
+    func testSetDSNWithBlankURL() {
+        let config = RavenConfig(DSN: "")
+        XCTAssertNil(config, "The config initialized")
     }
 }
